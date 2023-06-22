ea77c6c2a423659fe698c9e4cdf3d95b18fab6ab
Valentin Laurent <laurent.vltn@gmail.com>
2023-06-22 22:28:40 +0200

    Cults and panths tempalting

--- src/Components/Univers/Cults/Cults.js
+++ src/Components/Univers/Cults/Cults.js
@@ -1,6 +1,22 @@
 import Gods from './Gods'
-export default (props) => {
+import { panth } from './panthsMock'
+const Cults = (props) => {
   return <div>
     <Gods></Gods>
+    <div>
+      <div className="title-section">
+        <h3>Panhtéon & Cultes</h3>
+      </div>
+      <div className='panth-container'>
+        {panth.map((v) => <div className='panth-card'>
+          <img src={v.img}></img>
+          <div className="dark-filter"></div>
+          <div className='corner' style={{backgroundColor: v.color}}></div>
+          <p>{v.name}</p>
+        </div>)}
+      </div>
+    </div>
 </div>
 }
+
+export default Cults;
